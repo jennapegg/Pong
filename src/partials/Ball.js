e0cac1a1dbc5babbaead02ce2babe2acb4c12c81
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-02 09:44:54 -0700

    add vector movement to Ball.js

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -1,5 +1,5 @@
 import { SVG_NS } from "../settings";
-import Paddle from "./Paddle";
+// import Paddle from "./Paddle";
 
 export default class Ball {
     constructor(radius, boardWidth, boardHeight,) {
@@ -7,15 +7,32 @@ export default class Ball {
       this.boardWidth = boardWidth;
       this.boardHeight = boardHeight;
       this.direction = 1;
+
+      this.reset();
+    }
+
+    reset() {
+      this.x = this.boardWidth / 2;
+      this.y = this.boardHeight / 2;
+
+      this.vy = 0;
+      while(this.vy === 0) {
+        this.vy = Math.floor(Math.random() * 10 - 5);
+      }
+
+      this.vx = this.direction * (6 - Math.abs(this.vy));
     }
 
     render(svg) {
+        this.x += this.vx;
+        this.y += this.vy;
+        
         let ball = document.createElementNS(SVG_NS, 'circle');
 
         ball.setAttributeNS(null, 'fill', '#FFF');
         ball.setAttributeNS(null, 'r', this.radius);
-        ball.setAttributeNS(null, 'cx', this.boardWidth / 2);
-        ball.setAttributeNS(null, 'cy', this.boardHeight / 2);
+        ball.setAttributeNS(null, 'cx', this.x);
+        ball.setAttributeNS(null, 'cy', this.y);
 
         svg.appendChild(ball);
     }
